59c9c737cbf2f8ac82840430e2826f0336a73527
Mike <firedem0n@hotmail.com>
2016-01-31 09:26:06 -0700

    Adds some input handling.

--- dots.js
+++ dots.js
@@ -2,9 +2,15 @@ var dotRadius = 13;
 var dotSpacing = 18;
 var gridScale = 2*dotRadius + dotSpacing;
 var dotOffset = dotRadius + dotSpacing;
+var curDragList = [];
+var mouseDown = false;
 
 var gridToCoord = function (gridInd) {
     return gridScale * gridInd + dotOffset;
+}
+
+var coordToGrid = function (coord) {
+    return Math.round((coord - dotOffset) / gridScale);
 }
 
 var drawDot = function (gridX, gridY, color) {
@@ -16,6 +22,49 @@ var drawDot = function (gridX, gridY, color) {
 
 var canvas = document.getElementById("cnv");
 var ctx = canvas.getContext("2d");
+
+var canvasPos = {
+    x: canvas.offsetLeft - canvas.scrollLeft + canvas.clientLeft,
+    y: canvas.offsetTop - canvas.scrollTop + canvas.clientTop
+};
+
+var getGridPosFromEvent = function(e) {
+    var mouse = {
+        x: e.pageX - canvasPos.x,
+        y: e.pageY - canvasPos.y
+    };
+
+    var gridPos = {
+        x: coordToGrid(mouse.x),
+        y: coordToGrid(mouse.y)
+    };
+
+    return gridPos;
+};
+
+canvas.addEventListener('mousedown', function(e) {
+    curDragList = [];
+    mouseDown = true;
+});
+
+canvas.addEventListener('mouseup', function(e) {
+    mouseDown = false;
+    for (i = 0; i < curDragList.length; i++) {
+        drawDot(curDragList[i].x, curDragList[i].y, "#FF00F0");
+    }
+    curDragList = [];
+});
+
+canvas.addEventListener('mousemove', function(e) {
+    if (mouseDown) {
+        gridPos = getGridPosFromEvent(e);
+        curDragList.push(gridPos);
+    }
+
+    // renderDragList(curDragList);
+});
+
+
 drawDot(5, 5, "#00FF00");
 drawDot(5, 6, "#00FF00");
 drawDot(6, 6, "#00FF00");
